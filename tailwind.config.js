f38c05d5db3913a5325be55611cbe5d85c99b8e9
Antoine-Developer <antoinepenadev@gmail.com>
2025-04-30 13:09:23 -0400

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#121212",
+        title: "EAEAEA",
+        subtitle: "#B0BEC5",
+        secondary: "#1E1E1E",
+        btn: "#9C27B0",
+      }
+    },
+  },
+  plugins: [],
+}
+
