5f1440581cca56e9fc9474c532c888475d183138
Antoine-Developer <antoinepenadev@gmail.com>
2025-04-30 14:10:57 -0400

    Add deploy support on gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://antoinepdev/.github.io/cinee",
+  site: "https://antoinepdev.github.io",
+  base: "cinee",
   integrations: [tailwind(), sitemap()],
 });
